f0c6e391c6f014ae751601dc66f3798fae66a7d7
Harrison Wilkinson <cowilkin@Harrisons-MacBook-Pro.local>
2023-04-07 19:42:50 -0500

    can confirm - test 2 doesnt work on either my branch or the other branch

--- tests/integration/test_mutiny_integration.py
+++ tests/integration/test_mutiny_integration.py
@@ -79,60 +79,60 @@ class IntegrationSuite(object):
         self.passed_tests += 1
         print('ok')
 
-    # def test_2(self, target_port, proto, prepped_fuzzer_file):
-    #     '''
-    #     test details:
-    #         - prepped_fuzz: ./tests/assets/integration_test_2/<proto>_prepped.fuzzer
-    #         - target_host: 127.0.0.1
-    #         - sleep_time: 0
-    #         - range: None
-    #         - loop: None
-    #         - dump_raw: 0
-    #         - quiet: False
-    #         - log_all: False
-    #         - processor_dir: ./tests/assets/integration_test_2/
-    #         - failure_threshold: 3
-    #         - failure_timeout: 5.0
-    #         - receive_timeout: 3.0
-    #         - should_perform_test_run 1
-    #         - port: 7768-7771, unique for each test to avoid 'Address already in use OSError'
-    #         - source_port: -1
-    #         - source_ip: 0.0.0.0
+    def test_2(self, target_port, proto, prepped_fuzzer_file):
+        '''
+        test details:
+            - prepped_fuzz: ./tests/assets/integration_test_2/<proto>_prepped.fuzzer
+            - target_host: 127.0.0.1
+            - sleep_time: 0
+            - range: None
+            - loop: None
+            - dump_raw: 0
+            - quiet: False
+            - log_all: False
+            - processor_dir: ./tests/assets/integration_test_2/
+            - failure_threshold: 3
+            - failure_timeout: 5.0
+            - receive_timeout: 3.0
+            - should_perform_test_run 1
+            - port: 7768-7771, unique for each test to avoid 'Address already in use OSError'
+            - source_port: -1
+            - source_ip: 0.0.0.0
 
-    #         Fuzzes a target until it finds a 'crash' at seed=10, using a single
-    #         outbound line to test against regression on the bug described in issue #11,
-    #         upon reception of the crash, the monitor sends a HaltException to mutiny to halt execution
-    #     '''
-    #     self.total_tests += 1
-    #     print('test 2: {}'.format(proto))
-    #     #self.block_print() 
-    #     # populate args
-    #     args = Namespace(prepped_fuzz = prepped_fuzzer_file, target_host = self.target_if, sleep_time = 0, range = '0-', loop = None, dump_raw = None, quiet = False, log_all = False, testing = True, server=False)
+            Fuzzes a target until it finds a 'crash' at seed=10, using a single
+            outbound line to test against regression on the bug described in issue #11,
+            upon reception of the crash, the monitor sends a HaltException to mutiny to halt execution
+        '''
+        self.total_tests += 1
+        print('test 2: {}'.format(proto))
+        #self.block_print() 
+        # populate args
+        args = Namespace(prepped_fuzz = prepped_fuzzer_file, target_host = self.target_if, sleep_time = 0, range = '0-', loop = None, dump_raw = None, quiet = False, log_all = False, testing = True, server=False)
 
-    #     log_dir = prepped_fuzzer_file.split('.')[0] + '_logs'
-    #     # stand up target server
-    #     target = Target2(proto, self.target_if, target_port)
-    #     # run mutiny
-    #     fuzzer = Mutiny(args)
-    #     fuzzer.radamsa = os.path.abspath( os.path.join(__file__, '../../../radamsa-0.6/bin/radamsa'))
-    #     fuzzer.import_custom_processors()
-    #     fuzzer.debug = False
-    #     # start listening for the fuzz sessions
-    #     target_thread = threading.Thread(target=target.accept_fuzz, args=())
-    #     target_thread.start()
-    #     time.sleep(.03) # avoid race with connection to socket
-    #     fuzz_thread = threading.Thread(target=fuzzer.fuzz, args=())
-    #     fuzz_thread.start() # connect to target and begin fuzzing
-    #     target_thread.join()
-    #     if target.communication_conn:
-    #         target.communication_conn.close()
-    #     else:
-    #         target.listen_conn.close()
-    #     target.listen_conn.close()
-    #     shutil.rmtree(log_dir)
-    #     self.enable_print()
-    #     self.passed_tests += 1
-    #     print('ok')
+        log_dir = prepped_fuzzer_file.split('.')[0] + '_logs'
+        # stand up target server
+        target = Target2(proto, self.target_if, target_port)
+        # run mutiny
+        fuzzer = Mutiny(args)
+        fuzzer.radamsa = os.path.abspath( os.path.join(__file__, '../../../radamsa-0.6/bin/radamsa'))
+        fuzzer.import_custom_processors()
+        fuzzer.debug = False
+        # start listening for the fuzz sessions
+        target_thread = threading.Thread(target=target.accept_fuzz, args=())
+        target_thread.start()
+        time.sleep(.03) # avoid race with connection to socket
+        fuzz_thread = threading.Thread(target=fuzzer.fuzz, args=())
+        fuzz_thread.start() # connect to target and begin fuzzing
+        target_thread.join()
+        if target.communication_conn:
+            target.communication_conn.close()
+        else:
+            target.listen_conn.close()
+        target.listen_conn.close()
+        shutil.rmtree(log_dir)
+        self.enable_print()
+        self.passed_tests += 1
+        print('ok')
 
 
     def test_3(self, target_port, proto, prepped_fuzzer_file):
