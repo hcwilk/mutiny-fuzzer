9268cfca7c38ea2631a04c9ca82798bef6b45460
Harrison Wilkinson <cole@0xcoast.com>
2023-07-19 16:01:26 -0500

    cleaning up showcase documentation

--- demo/show/harness/harness.py
+++ demo/show/harness/harness.py
@@ -1,9 +1,16 @@
 import subprocess
 import time
+import sys
 
 def main():
-    with open('inputs.txt', 'r') as file:
+    if len(sys.argv) < 2:
+        print("Usage: python3 script.py <filename>")
+        return
+    filename = sys.argv[1]
+    with open(filename, 'r') as file:
         lines = file.readlines()
+
+    crash_found = False  
 
     for line in lines:
         line = line.strip()
@@ -13,7 +20,10 @@ def main():
             time.sleep(.5)
         except subprocess.CalledProcessError as e:
             print(f"Crashed with input: {line}")
+            crash_found = True
             break  # Stop fuzzing after a crash
+    if not crash_found:
+        print("Done fuzzing, no crashes found!")
 
 if __name__ == "__main__":
     main()
